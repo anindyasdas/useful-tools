9d4c5666251ec66bc795e046143f3b493c3708d9
anindyasdas <50780535+anindyasdas@users.noreply.github.com>
2020-05-07 02:24:14 +0530

    Update fidpytorch.py

--- fidpytorch.py
+++ fidpytorch.py
@@ -2,7 +2,7 @@
 """
 Created on Tue May  5 17:49:51 2020
 
-@author: Anindya
+
 """
 
 #!/usr/bin/env python3
@@ -22,22 +22,7 @@ samples respectively.
 
 See --help to see further details.
 
-Code apapted from https://github.com/bioinf-jku/TTUR to use PyTorch instead
-of Tensorflow
 
-Copyright 2018 Institute of Bioinformatics, JKU Linz
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
 import os
 import pathlib
